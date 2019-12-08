9c8c4d45f6efbd9416d43d85b10baf26e337b00d
adhariari <adhari.ariari@gmail.com>
2019-12-08 14:02:04 +0700

    visibility

--- visibility.php
+++ visibility.php
@@ -4,26 +4,32 @@ class Produk
 {
     public $judul,
     $penulis,
-    $penerbit,
-        $harga;
+    $penerbit;
+    protected $diskon = 0;
+
+    private $harga;
 
     public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
     {
-        $this->a = $judul;
-        $this->b = $penulis;
-        $this->c = $penerbit;
-        $this->d = $harga;
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
     }
 
+    public function getHarga()
+    {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
     public function getLabel()
     {
-        return "$this->b, $this->c";
+        return "$this->penulis, $this->penerbit";
 
     }
 
     public function getInfoProduk()
     {
-        $str = "{$this->a} | {$this->getLabel()} (Rp. {$this->d})";
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
         return $str;
     }
 
@@ -44,21 +50,30 @@ class Komik extends Produk
 ";
         return $str;
     }
+        public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+
 }
 
-class Game extends Produk 
+class Game extends Produk
 {
     public $waktuMain;
     public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0)
     {
         parent::__construct($judul, $penulis, $penerbit, $harga);
         $this->waktuMain = $waktuMain;
-    } 
+    }
     public function getInfoProduk()
     {
         $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.
 ";
         return $str;
+    }
+        public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
     }
 }
 
@@ -66,15 +81,27 @@ class CetakInfoProduk
 {
     public function cetak(Produk $produk)
     {
-        $str = "{$produk->a} | {$produk->getLabel()} (Rp. {$produk->d})";
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
         return $str;
     }
 
 }
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 200, 0);
-$produk2 = new Game("Dota 2", "Gaben", "Valve", "Free", 0, 3);
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 200);
+$produk2 = new Game("Dota 2", "Gaben", "Valve", 80000, 3);
 
 echo $produk1->getInfoProduk();
 echo "<br>";
 echo $produk2->getInfoProduk();
+echo "<hr>";
+
+// $produk2->harga = 5000;
+$produk1->setDiskon(50);
+$produk2->setDiskon(30);
+echo $produk1->getHarga();
+echo"<br>";
+echo $produk2->getHarga();
+
+// var_dump($produk1);
+// var_dump($produk2);
+
