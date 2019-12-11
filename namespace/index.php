ecc527b50c64ac4a78998f9345a742d3d1e6af26
adhariari <adhari.ariari@gmail.com>
2019-12-11 23:21:11 +0700

    namespaces

---
+++ namespace/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'App/init.php';
+
+// $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 200);
+// $produk2 = new Game("Dota 2", "Gaben", "Valve", 80000, 3);
+
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+
+// echo $cetakProduk->cetak();
+// echo "<hr>";
+
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
+new ServiceUser();
+echo "<br>";
+new ProdukUser();
